e513306b7c906fa6b386e8a54c3c53caaa75f1fc
Hiteshmeher <meher.hitesh4@gmail.com>
2022-09-08 21:46:14 +0530

    [Hitesh]: added code for returning Happy if in any mood except sad

--- MoodAnalyzer/src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ MoodAnalyzer/src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,15 +1,25 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
+    /**
+     *
+     * @param message
+     * @return
+     */
     public static String analyzeMood(String message){
         if (message.contains("sad"))
+            // if message contains sad it will return sad
             return "SAD";
-        else if (message.contains("happy"))
+        else // if message will not contain sad then it will return Happy
             return "HAPPY";
-        else
-            return null;
     }
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+
        MoodAnalyzer mood = new MoodAnalyzer();
        System.out.println(mood.analyzeMood("i am in sad mood"));
        System.out.println(mood.analyzeMood("i am in happy mood"));
